ed8b665f4a43714da5a2c2caede83ce8e3539b57
Kyle <kyle@elshimmer.com>
2022-09-10 12:54:43 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ from collections import namedtuple
 import altair as alt
 import math
 import pandas as pd
-import streamlit as st
-from gsheetsdb import connect
+import streamlit
 
 
 """
@@ -41,18 +40,3 @@ with st.echo(code_location='below'):
     
     # Create a connection object.
 conn = connect()
-
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["public_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
-# Print results.
-for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
